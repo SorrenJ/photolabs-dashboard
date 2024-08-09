dacd8b5535e4ae41d7d616879df9099c57996198
SorrenJ <soalexjao@gmail.com>
2024-08-09 15:17:06 -0700

    commented dashboard compontent without classes, add loading component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 
+
+// import React from "react";
 import classnames from "classnames";
+
 
 class Dashboard extends Component {
   render() {
     const dashboardClasses = classnames("dashboard");
-
     return <main className={dashboardClasses} />;
   }
 }
+
+
+// If we wrote the same component without classes, it would perform the equivalent behaviour and return the same value.
+// function Dashboard(props) {
+//   const dashboardClasses = classnames("dashboard");
+//   return <main className={dashboardClasses} />;
+// }
 
 export default Dashboard;
