9b794e39b5bb43f037229f3bcdc2d9b02ebc858b
SorrenJ <soalexjao@gmail.com>
2024-08-09 15:57:52 -0700

    import Loading component to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 
+import Loading from './Loading';
 
 // import React from "react";
 import classnames from "classnames";
 
 
 class Dashboard extends Component {
+  //It should default to true because the application will start loading data immediately after the components render for the first time
+  
+  state = {
+    loading: true
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    // When the value of this.state.loading is true we want to render the loading indicator conditionally
+    if (this.state.loading) {
+      return <Loading />;
+    }
+
     return <main className={dashboardClasses} />;
   }
 }
-
-
-// If we wrote the same component without classes, it would perform the equivalent behaviour and return the same value.
-// function Dashboard(props) {
-//   const dashboardClasses = classnames("dashboard");
-//   return <main className={dashboardClasses} />;
-// }
-
 export default Dashboard;
