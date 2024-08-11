89b81af4be7b7abc8c8baf4459074e919677cf41
SorrenJ <soalexjao@gmail.com>
2024-08-10 19:46:01 -0700

    add state handling for selecting a panel id, result in TypeError with setState will fix with next commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -34,11 +34,30 @@ class Dashboard extends Component {
   //It should default to true because the application will start loading data immediately after the components render for the first time
   
   state = {
-    loading: false // false simply disables the loading screen
+    loading: false, // false simply disables the loading screen
+   
+   
+    focused: null // When we are in focused mode, we don't want to render four panels; we want to render one
+  /* If this.state.focused is null then return true for every panel.
+If this.state.focused is equal to the Panel, then let it through the filter.*/
+  
   };
 
+
+  selectPanel(id) {
+    this.setState({
+     focused: id
+    });
+   }
+  
+
+
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
 
     // When the value of this.state.loading is true we want to render the loading indicator conditionally
     if (this.state.loading) {
@@ -46,15 +65,17 @@ class Dashboard extends Component {
     }
 
  
-    const panels = data.map(panel => (
-      <Panel
-        key={panel.id}
-        id={panel.id}
-        label={panel.label}
-        value={panel.value}
-      />
+    // Use the this.state.focused value to filter panel data before converting it to components.
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map(panel => (
+     <Panel
+      key={panel.id}
+      id={panel.id}
+      label={panel.label}
+      value={panel.value}
+      onSelect={this.selectPanel}
+     />
     ));
- 
  
     return <main className={dashboardClasses}>{panels}</main>;
   }
